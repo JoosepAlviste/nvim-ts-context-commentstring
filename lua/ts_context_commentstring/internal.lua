cd0098b7fb7fe1337dfb80060fd08ad6996207df
Daniel Woznicki <danwoz@nettoolkit.com>
2022-03-18 12:14:00 -0700

    Fixed comment string inside JSX spread element

--- lua/ts_context_commentstring/internal.lua
+++ lua/ts_context_commentstring/internal.lua
@@ -70,6 +70,7 @@ M.config = {
     comment = { __default = '// %s', __multiline = '/* %s */' },
     call_expression = { __default = '// %s', __multiline = '/* %s */' },
     statement_block = { __default = '// %s', __multiline = '/* %s */' },
+    spread_element = { __default = '// %s', __multiline = '/* %s */' },
   },
 }
 
