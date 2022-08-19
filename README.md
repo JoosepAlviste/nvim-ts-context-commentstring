37a97a04c39f26fffe7745815517e1ce1a0eb3be
Joosep Alviste <joosep.alviste@gmail.com>
2022-08-19 20:16:59 +0300

    Add Comment.nvim integration code into this plugin
    This should make it simpler for users to set up the integration, as well
    as allow us to add more features to it in the future. Users wouldn't
    need to maintain the integration themselves.
    
    This does mean that if there are any breaking changes inside
    Comment.nvim, we would need to release a fix for the integration fast.

--- README.md
+++ README.md
@@ -330,26 +330,11 @@ require'nvim-treesitter.configs'.setup {
 ```
 
 Then, configure `Comment.nvim` to trigger the `commentstring` updating logic 
-with its `pre_hook` configuration (note that we can add some extra logic to 
-improve the integration granularity):
+with its `pre_hook` configuration:
 
 ```lua
 require('Comment').setup {
-  pre_hook = function(ctx)
-    local U = require 'Comment.utils'
-
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require('ts_context_commentstring.utils').get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require('ts_context_commentstring.utils').get_visual_start_location()
-    end
-
-    return require('ts_context_commentstring.internal').calculate_commentstring {
-      key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
-      location = location,
-    }
-  end,
+  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 }
 ```
 
---
+++ lua/ts_context_commentstring/integrations/comment_nvim.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+---Use this function to get a pre_hook function that can be used when
+---configuring Comment.nvim.
+---https://github.com/numToStr/Comment.nvim/
+---
+---Example usage:
+---```lua
+---require('Comment').setup {
+---  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+---}
+---```
+---
+---Feel free to copy this function into your own configuration if you need to
+---make any changes (or contribute the improvements back into this plugin).
+---
+---This is a higher order function in case we want to add any extra
+---configuration for the hook in the future.
+---
+---@return fun(ctx: CommentCtx): string|nil
+function M.create_pre_hook()
+  ---@param ctx CommentCtx
+  ---@return string|nil
+  return function(ctx)
+    local U = require 'Comment.utils'
+
+    -- Determine whether to use linewise or blockwise commentstring
+    local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
+
+    -- Determine the location where to calculate commentstring from
+    local location = nil
+    if ctx.ctype == U.ctype.blockwise then
+      location = require('ts_context_commentstring.utils').get_cursor_location()
+    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+      location = require('ts_context_commentstring.utils').get_visual_start_location()
+    end
+
+    return require('ts_context_commentstring.internal').calculate_commentstring {
+      key = type,
+      location = location,
+    }
+  end
+end
+
+return M
